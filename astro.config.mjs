c6b9fce028a535b68fb33530d73b6b3bfad23278
JGH Inled Group <github@inled.es>
2025-04-17 10:07:20 +0200

    Listo para lanzarse

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://inled-group.github.io/matrixcv',
+    base: 'matrixcv',
+
+});
